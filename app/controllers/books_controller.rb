054beb356e293099e4044a8d8061c3b4be951352
vagrant <vagrant@localhost.localdomain>
2020-07-22 04:21:38 +0000

    [Add] success and error messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to @book
+      #flash[:notice] = "Book was successfully created."
+      redirect_to @book, notice: "Book was successfully created."
     else
       @books = Book.all
       render 'index'
@@ -24,8 +25,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to @book
+    if @book.update(book_params)
+      redirect_to @book, notice: "Book was successfully updated."
+    else
+      render 'edit'
+    end
   end
 
   def destroy
